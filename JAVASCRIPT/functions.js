410971d9a2a19afb45cf7765c5eb824ee61ad10b
Karol12400 <garciamartinezkarolelias@gmail.com>
2024-02-01 12:30:16 -0600

    Se agregaron algunas lineas de codigo en JavaScript para mi perfil

---
+++ JAVASCRIPT/functions.js
@@ -0,0 +1,5 @@
+function changeColor (){
+       document.getElementById("palabrita").style.backgroundColor="green"
+      document.getElementById("palabrita").style.color="white";
+      document.getElementById("Tablita").style.backgroundColor="Red";
+}
