f79eb9736bccbc8c07336e69a833366f1b4a028c
Karol12400 <garciamartinezkarolelias@gmail.com>
2024-04-09 10:50:32 -0600

    Codigos del curso en linea

---
+++ Curso en linea/repaso java-script/main.js
@@ -0,0 +1,18 @@
+//Alertas
+//alert("Hola soy Victor Robles WEB");
+
+//Variables
+let nombre = "Victor Robles";
+nombre = "victor";
+
+//Constantes
+const apellido = "robles";
+const altura = 187;
+
+//Mostrar por consola
+console.log(nombre);
+console.log(altura);
+
+//Concanetacion
+let concatenacion = nombre + " " + altura;
+console.log(concatenacion);
